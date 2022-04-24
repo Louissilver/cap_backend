a150d9afb81d1a6900777cac56f95e93f7e24bfb
Luís Fernando da Silveira <luisfernandosilveira23@gmail.com>
2022-04-24 20:59:50 -0300

    hotfix: Ajuste na chamada do cors no use

--- src/server.js
+++ src/server.js
@@ -1,17 +1,19 @@
+require('dotenv').config();
+
 const express = require('express');
-const conn = require('./database');
 const routes = require('./routes');
+const conn = require('./database');
 const cors = require('cors');
+
+conn();
 
 const app = express();
 const port = 3333;
 
-app.use(cors);
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
 app.listen(port, () => {
   console.log(`🔆 Aplicação rodando na URL https://localhost:${port}`);
 });
-
-conn();
